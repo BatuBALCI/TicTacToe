89126ca5cdfa71880d8f4d4e2dd265677239c774
BatuBALCI <bbalci@larsa4d.com>
2022-04-30 23:52:53 +0300

    Add IMGUI source files as a new VS project

--- TicTacToeGraphicEngine/TicTacToeGraphicEngine/LearnOPENGL_Initialize.cpp
+++ TicTacToeGraphicEngine/TicTacToeGraphicEngine/LearnOPENGL_Initialize.cpp
@@ -10,6 +10,16 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 
 #include "Mesh.h"
 #include "Shader.h"
@@ -118,6 +128,41 @@ int main(void)
     GLuint uniformEyeLocationLocation = 0;
 
 
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window.GetMainWindow(), true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     // loop until the window close
     while (!window.GetShouldClose())
     {
@@ -130,36 +175,61 @@ int main(void)
         // Get and handle user input events
         glfwPollEvents();
 
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        ImGui::Render();
+
         // get keys from window and pass it into keyControl method to control the camera.
         camera.keyControl(window.GetKeys(), deltaTime);
         // get xChange and yChange values from window and pass it into mouseControl method to control the camera.
         camera.mouseControl(window.GetXChange(), window.GetYChange());
 
-        if (direction)
-        {
-            triOffset += triIncrement;
-        }
-        else 
-        {
-            triOffset -= triIncrement;
-        }
-
-        if (abs(triOffset) >= triMaxOffset) 
-        {
-            direction = !direction;
-        }
-
-        curAngle += 0.05f;
-
-        if (curAngle>=360)
-        {
-            curAngle -= 360;
-        }
-
         // Set every pixel in the frame buffer to the current clear color.
         // alpha means visibility
-        glClearColor(0.2f, 0.0f, 0.0f, 0.3f);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         // This function opens the shader inside the program and until it closes program uses attached shader
         shaderList.at(0)->UseShader();
@@ -180,12 +250,12 @@ int main(void)
 
         // 4x4 matrix with zeros;
         glm::mat4 model, projection;
-        //First apply translation than apply rotation glm automatically combine this two movement.
+        // First apply translation than apply rotation glm automatically combine this two movement.
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
         model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
         projection = glm::perspective(30.0f, (GLfloat)bufferWidth/(GLfloat)bufferHeight, 0.1f, 100.0f);
-        //Since we have pointer of a matrix we didn't directly pass model object we pass the pointer of it.
+        // Since we have pointer of a matrix we didn't directly pass model object we pass the pointer of it.
         glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.CalculateViewMatrix()));
         glUniformMatrix4fv(uniformModel,1,GL_FALSE,glm::value_ptr(model));
@@ -194,7 +264,7 @@ int main(void)
         // use lights for the objects that are going to be rendered.
         mainLight.UseLight(uniformAmbientIntensityLocation, uniformAmbientColourLocation);
         diffuseLight.UseLight(uniformDiffuseIntensityLocation, uniformDiffuseColourLocation, uniformDirectionLocation);
-        //material.UseMaterial(uniformSpecularIntensityLocation, uniformShininessLocation);
+        // material.UseMaterial(uniformSpecularIntensityLocation, uniformShininessLocation);
         // call renderMesh function to draw the element on the screen.
         meshList.at(0)->RenderMesh();
         
@@ -202,6 +272,11 @@ int main(void)
 
         window.swapBuffers();
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     return 0;
 }
